278f94022d90ed07a77073f97dc8d69d69100b63
root <root@BOOK-QTP3KTPNKO>
2023-11-24 15:11:02 -0800

    Edit tests README for my own reference, clean up tests

--- brevets/tests/README.md
+++ brevets/tests/README.md
@@ -3,3 +3,13 @@
 Write 5 test cases for `../acp_times.py` in `test_acp_times.py`. Remeber: you have to write them based on the original website.
 
 Also remember you must run the tests from the parent directory to access `acp_times`. You can just use `run_tests.sh` for that, like in project 3.
+
+## Setup
+
+Jump into your container and run `./run_tests.sh`.
+
+```shell
+docker exec -it YOUR_CONTAINER_ID /bin/bash
+```
+
+Note that you should not enter the tests directory, `run_tests.sh` will take care of everything.
--- brevets/tests/test_acp_times.py
+++ brevets/tests/test_acp_times.py
@@ -7,24 +7,6 @@ Write your tests HERE AND ONLY HERE.
 # (put them in `tests`, follow Project 3 if necessary) and 
 # all pass.
 
-
-
-
-
-# test if the last distance is less than the total distance, 
-# an exception is thrown
-#     ex: the last control point (120.00 km) must be at least 200 km
-
-# test if the last distance is MORE than 20% of the total distance, 
-# an exception is thrown
-#     ex: the last control point (250.00 km) is over 20% longer than 
-#         the theoretical distance (200 km): error in selecting units?
-
-# test if the last distance less than OR equal to 20% more than the total distance, 
-# no exception is thown, and calculation is based on total distance and not last distance
-
-
-
 import nose    # Testing framework
 import logging
 import arrow
@@ -68,4 +50,3 @@ def test_0():
     assert acp.open_time(0.0, brevet, start) == start
     # since 0 < 60, this control is relaxed, so min 1hr before close
     assert acp.close_time(0.0, brevet, start) == start.shift(hours=1)
-
